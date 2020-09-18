89eb00cbfdcb4e214bd3c9c65f3cea0dc299bb74
thawngbaw <thawngbaw@urbandaleschools.com>
2020-09-18 12:16:49 -0500

    trapezoid

--- 3.2_Trapezoid.py
+++ 3.2_Trapezoid.py
@@ -10,3 +10,8 @@ base 1: 5       base 2: 7    height: 2    area: 12
 base 1: 1       base 2: 2    height: 3    area: 4.5
 base 1: 7       base 2: 2    height: 4    area: 18
 '''
+base= float (input("enter the base value: "))
+base2=float (input ("enter the base 2 value: "))
+height=float(input ("enter the height value: "))
+mpg= (base + base2) / 2 * height
+print (mpg)
